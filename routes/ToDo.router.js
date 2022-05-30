8076e6e7b7eb3e6dd77269fff3eacf1fb7e2f2c5
Juan Villegas <jvillegas422@gmail.com>
2022-05-29 19:23:11 -0500

    Added GET again

--- routes/ToDo.router.js
+++ routes/ToDo.router.js
@@ -3,9 +3,24 @@ const pool = require('../modules/pool')
 const ToDoRouter = express.Router();
 // DB CONNECTION
 
+ToDoRouter.get('/', (req, res) => {{
+    const sqlQuery = `
+        SELECT * FROM task
+        ORDER BY "name" ASC
+    `;
 
+    pool.query(sqlQuery)
+        .then((dbRes) => {
+            console.log('DB request success', dbRes.rows);
+            res.send(dbRes.rows);
+        })
+        .catch((err) => {
+            console.log(`DB request failed`, err);
 
+            res.sendStatus(500);
+        });
+}});
 
-
+    
 
 module.exports = ToDoRouter;
