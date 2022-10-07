e72006a3dc9e6c114d5cb42f2c9888e4a2e6afb8
Juan Villegas <jvillegas422@gmail.com>
2022-10-07 09:47:06 -0500

    Add client.js base structure

---
+++ server/public/scripts/script.js
@@ -0,0 +1,35 @@
+console.log('JS is working!');
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('in JQ!');
+    // Add click listeners here 👇
+
+
+};
+
+function getTask() {
+    console.log('in GET task');
+
+};
+
+function postTask() {
+    console.log('in POST task');
+
+};
+
+function appendTask() {
+    console.log('in appendTask!');
+
+};
+
+function deleteTask() {
+    console.log('in deleteTask!');
+
+};
+
+function updateTask() {
+    console.log('in updateTask!');
+
+};
